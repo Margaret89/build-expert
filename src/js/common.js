6fb257e8597ea0dfd391ec1126df08a0cb3a4240
Margaret <match14@yandex.ru>
2022-07-25 23:44:48 +0300

    header + footer

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 
-// import 'select2';
-// import { Fancybox } from "@fancyapps/ui";
-// import 'slick-slider/slick/slick.min.js';
-// import Inputmask from "inputmask";
+import { Fancybox } from "@fancyapps/ui";
+import 'slick-slider/slick/slick.min.js';
+import Inputmask from "inputmask";
 
-export {$};
+window.Fancybox = Fancybox;
+
+export {$, Inputmask, Fancybox};
